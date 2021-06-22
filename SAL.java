45ac8f3666e260b11d1e7d45785baffc04ffd593
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 23:12:25 +0530

    Added player position

--- SAL.java
+++ SAL.java
@@ -3,5 +3,7 @@ public class SAL
 public static void main(String[] args)
 {
 System.out.println("welcome to snake and ladder program");
+int playerposition = 0;
+System.out.println("playerposition is : " +playerposition);
 }
 }
