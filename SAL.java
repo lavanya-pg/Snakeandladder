6a050d15bf77f52849bc92a9a1429c4393b39953
lavanya-pg <lavanyapg8@gmail.com>
2021-06-23 00:24:54 +0530

    Assigning winningposition

--- SAL.java
+++ SAL.java
@@ -4,27 +4,30 @@ public class SAL
 {
 public static void main(String[] args)
 {
-int PlayerPosition=1;
-
+int PlayerPosition=0;
+int WinningPosition=100;
 System.out.println("Welcome to Snake And Ladder");
+while(WinningPosition>PlayerPosition)
+{
 int die = (int) (Math.floor( Math.random() * 10) % 6+1);
 System.out.println("Die rolled is :" +die);
 int option =(int) (Math.floor(Math.random() * 10)%3);
 switch(option)
 {
 case 0:
-System.out.println("No play");
+System.out.println("No play" +0);
 PlayerPosition += 0;
 break;
 case 1:
-System.out.println("Ladder");
+System.out.println("Ladder" +die);
 PlayerPosition += die;
 break;
 default:
-System.out.println("Snake");
+System.out.println("Snake"+die);
 PlayerPosition -= die;
 if(PlayerPosition<0)
 PlayerPosition=0;
+}
 }
 System.out.println("Player position is : "+PlayerPosition);
 }
