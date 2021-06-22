b08d886399db25af075490f4603708cf597b8254
lavanya-pg <lavanyapg8@gmail.com>
2021-06-23 00:02:13 +0530

    using cases to check the playerposition

--- SAL.java
+++ SAL.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class SAL
 {
 public static void main(String[] args)
 {
-System.out.println("welcome to snake and ladder program");
-int playerposition = 1;
-System.out.println("playerposition is : " +playerposition);
-int dice = (int) (Math.floor(Math.random() * 10) % 6+1);
-System.out.println("dice rolled is : " +dice);
+int PlayerPosition=1;
+
+System.out.println("Welcome to Snake And Ladder");
+int die = (int) (Math.floor( Math.random() * 10) % 6+1);
+System.out.println("Die rolled is :" +die);
+int option =(int) (Math.floor(Math.random() * 10)%3);
+switch(option)
+{
+case 0:
+System.out.println("No play");
+PlayerPosition += 0;
+break;
+case 1:
+System.out.println("Ladder");
+PlayerPosition += die;
+break;
+default:
+System.out.println("Snake");
+PlayerPosition -= die;
+if(PlayerPosition<0)
+PlayerPosition=0;
+}
+System.out.println("Player position is : "+PlayerPosition);
 }
 }
