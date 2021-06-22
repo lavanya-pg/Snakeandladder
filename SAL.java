dc76ee316b4b9cd288a2f1fd2c4f25176e986b46
lavanya-pg <lavanyapg8@gmail.com>
2021-06-22 23:28:17 +0530

    Added: used random function to display the num between 1 to 6

--- SAL.java
+++ SAL.java
@@ -3,7 +3,9 @@ public class SAL
 public static void main(String[] args)
 {
 System.out.println("welcome to snake and ladder program");
-int playerposition = 0;
+int playerposition = 1;
 System.out.println("playerposition is : " +playerposition);
+int dice = (int) (Math.floor(Math.random() * 10) % 6+1);
+System.out.println("dice rolled is : " +dice);
 }
 }
