f56cd3f0bd4d57c03a8e5d13b81fb7e80a721bbf
lavanya-pg <lavanyapg8@gmail.com>
2021-06-23 01:02:10 +0530

    report the count of dice

--- SAL.java
+++ SAL.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class SAL
 {
-public static void main(String[] args)
-{
-int PlayerPosition=0;
-int WinningPosition=100;
+  public static void main(String[] args)
+  {
+    int PlayerPosition=0;
+    int WinningPosition=100;
+    int Diecount=0;
 System.out.println("Welcome to Snake And Ladder");
 while(WinningPosition>PlayerPosition)
 {
 int die = (int) (Math.floor( Math.random() * 10) % 6+1);
-
+DieCount++;
 System.out.println("Die rolled is :" +die);
 int RemainingPosition= WinningPosition-PlayerPosition;
 
@@ -36,5 +37,6 @@ PlayerPosition=0;
 }
 }
 System.out.println("Player position is : "+PlayerPosition);
+System.out.println("Dice rolled "+Diecount+"times");
 }
 }
